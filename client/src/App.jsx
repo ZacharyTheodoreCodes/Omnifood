c5fd36fd61a6385d067b8bdedaf85f90a8cce24d
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-01 16:11:52 +0700

    feat: setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+//import "./App.css";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
